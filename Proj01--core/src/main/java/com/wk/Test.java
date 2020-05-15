b0e7d99b94f64d8d32dd5f1ad52ac436c8ab33aa
devloper001 <devdummy@hotmail.com>
2020-05-15 18:45:44 +0530

    Project added by team lead

---
+++ Proj01--core/src/main/java/com/wk/Test.java
@@ -0,0 +1,14 @@
+package com.wk;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("wakil Kumar");
+		System.out.println("hello world");
+		
+
+	}
+
+}
