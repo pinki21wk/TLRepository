2f506c05372013677e08b18c092d4f3ef8998099
devloper001 <devdummy@hotmail.com>
2020-05-16 18:13:12 +0530

    tl commited

--- AopProj01/src/main/java/com/wk/test/AopBankServiceTest.java
+++ AopProj01/src/main/java/com/wk/test/AopBankServiceTest.java
@@ -1,9 +1,25 @@
 package com.wk.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.wk.service.BankService;
+
 public class AopBankServiceTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BankService proxy=null;
+		ApplicationContext ctx=null;
+		ctx=new ClassPathXmlApplicationContext("/com/wk/config/applicationContext.xml");
+		
+		
+		
+		 proxy=ctx.getBean("bankService",BankService.class);
+		 System.out.println(proxy.calCompoundIntr(100000, 2, 1));
+		 System.out.println(proxy.getClass()+"---------"+proxy.getClass().getSuperclass());
+		
+		
 
 	}
 
