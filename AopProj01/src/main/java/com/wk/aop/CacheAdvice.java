3eebbb5b34ae0549f9f57f5199b04a195f57921a
devloper001 <devdummy@hotmail.com>
2020-05-16 16:12:09 +0530

    commit

--- AopProj01/src/main/java/com/wk/aop/CacheAdvice.java
+++ AopProj01/src/main/java/com/wk/aop/CacheAdvice.java
@@ -10,7 +10,7 @@ public class CacheAdvice implements MethodInterceptor {
 	@Override
 	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
 		// TODO Auto-generated method stub
-		return null;
+		return "";
 	}
 	
 	
